f1a192aa387b1149cd9223675d08172f454c4a4b
Shun Chiang <yuushamenma@gmail.com>
2020-07-21 13:35:16 -0500

    finished

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,39 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    # Iterative
+    # start_index = start
+    # end_index = end
+
+    # while start_index <= end_index:
+    #     middle_index = (start_index+end_index)//2
+    #     middle_value = arr[middle_index]
+    #     if middle_value == target:
+    #         return middle_index
+    #     elif target < middle_value:
+    #         end_index = middle_index - 1
+    #     else:
+    #         start_index = middle_index + 1
+    # return -1
+    if start > end:
+        return -1
+    else:
+        mid = (start+end)//2
+        if target == arr[mid]:
+            return mid
+        elif target < arr[mid]:
+            return binary_search(arr, target, start, mid-1)
+        else:
+            return binary_search(arr, target, mid+1, end)
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
 
+
+def agnostic_binary_search(arr, target):
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,63 @@
+
+def partition(data):
+    pivot = data[0]
+    left = []
+    right = []
+
+    for current in data[1:]:
+        if current <= pivot:
+            left.append(current)
+        else:
+            right.append(current)
+    return left, right, pivot
+
+
+def quicksort(data):
+    if len(data) <= 1:
+        return data
+    left, right, pivot = partition(data)
+    return quicksort(left)+[pivot]+quicksort(right)
+
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+
+
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    # elements = len(arrA) + len(arrB)
+    # merged_arr = [0] * elements
+    merged_arr = []
 
     # Your code here
-
+    while arrA or arrB:
+        if not arrB or (arrA and arrA[0] < arrB[0]):
+            merged_arr.append(arrA.pop(0))
+        else:
+            merged_arr.append(arrB.pop(0))
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
+
 def merge_sort(arr):
     # Your code here
+    if len(arr) <= 1:
+        return arr
+    mid = int(len(arr) / 2)
+    arrA, arrB = merge_sort(arr[:mid]), merge_sort(arr[mid:])
 
+    return merge(arrA, arrB)
 
-    return arr
-
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
+
+
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
